5e0738d502aed0d8619aa35754aed3fb7a00331f
Hermosita <Helen-aquarius@yandex.ru>
2022-08-08 19:33:50 +0300

    add problem8

---
+++ задача_8/Program.cs
@@ -0,0 +1,27 @@
+﻿
+	            int i = 1;
+	            bool not = true;
+	
+
+	            Console.WriteLine("Введите число:");
+	             int N= Convert.ToInt32(Console.ReadLine());
+	
+
+	            Console.WriteLine("Чётные числа от 1 до " +  N);
+	            while (i <=  N)
+	            {
+	                if (i % 2 != 1)
+	                {
+	                    Console.Write(i + ", ");
+	                  not = false;
+	                }
+	                i++;
+	            }
+	
+
+	            if (not)
+	            {
+	                Console.WriteLine("Нет чётных чисел!");
+	            }
+	        
+
